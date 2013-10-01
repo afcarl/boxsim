a36782bd62940db4e679e22aff6b20df7797c010
Paul Fudal <paul.fudal@inria.fr>
2013-10-01 17:20:17 +0200

    classpath fixed to include sockit.jar in ext/

--- boxsim/tests/run_server.sh
+++ boxsim/tests/run_server.sh
@@ -1,4 +1,4 @@
 # this starts the java server only, for debugging purposes
 # the first argument is the port number.
 cd ../../;
-java -cp target/interact.jar experiments.interact.StandAlone $1
+java -cp target/interact.jar:ext/sockit.jar experiments.interact.StandAlone $1
