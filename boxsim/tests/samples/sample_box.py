0508382d6ebc0d646e4abe61eff2ce8116499201
humm <fabien.benureau+git@gmail.com>
2013-10-16 16:17:28 +0200

    updated samples

--- boxsim/tests/samples/sample_box.py
+++ boxsim/tests/samples/sample_box.py
@@ -1,13 +1,9 @@
 import testenv
-import random
-
 import boxsim
 from common import cfg
 
-box = boxsim.UniformizeSim(boxsim.BoxSim(cfg))
+box = boxsim.Simulation(cfg)
 
-#order = [random.uniform(bi_min, bi_max) for (bi_min, bi_max) in box.m_bounds]
-order = [0.69, -0.36, -0.19, -0.94, -0.07, -0.72, 0.65, -0.02, -0.36, 0.23, -0.75, -0.35, 0.91]
-print box.execute_order(order)
+print box.execute_order([0.685, 0.581, 0.725, 0.271, 0.752, 0.425, 0.252, 0.756, 0.262, 0.083, 0.290, 0.712, 0.699])
 
 box.close()
