f7cc078622808fb55e7d2e16aea55277d1dc070d
humm <fabien.benureau+git@gmail.com>
2013-10-29 14:43:49 +0100

    test for hearing primitive

---
+++ boxsim/tests/test_hear.py
@@ -0,0 +1,27 @@
+import testenv
+import random
+
+import boxsim
+from common import cfg
+
+cfg.com.visu = False
+
+cfg.toys.ball1.friction       = 1.0
+cfg.toys.ball1.restitution    = 1.0
+cfg.toys.ball1.density        = 0.5
+cfg.toys.ball1.linear_damping = 0.3
+
+cfg.sprimitive.name = 'hear'
+cfg.sprimitive.object_name = 'ball1'
+
+box = boxsim.Simulation(cfg)
+
+count = 0
+for _ in range(10000):
+    order = [random.random() for _ in range(13)]
+    feedback = box.execute_order(order)
+    if feedback[2] != 0.0:
+    	count += 1
+
+box.close()
+print("{}/{}".format(count, 1000))
